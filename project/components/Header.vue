88b1ebdee7e70d9b817174b2085b3cffa5b583cf
salmonindaeyo <iamfhaa@hotmail.com>
2023-05-21 21:22:54 +0700

    Update responsive header

--- project/components/Header.vue
+++ project/components/Header.vue
@@ -4,14 +4,14 @@
      <span class="font-semibold">Github</span> Jobs
   </div>
  
- <div class="relative bg-black flex justify-center items-center">
-  <img src="pic/backgroundImg.png" class="relative"> 
-  <div class="w-[790px] h-[55px] bg-white absolute rounded flex justify-between items-center pl-[17px] pr-1 text-[#B9BDCF]">
+ <div class="min-h-[132px] relative flex justify-center items-center">
+  <img src="pic/backgroundImg.png" class="relative min-h-[132px] rounded"> 
+  <div class="h-[55px] w-5/6 bg-white absolute rounded flex justify-between items-center pl-[17px] pr-1 text-[#B9BDCF]">
     <div class="flex justify-between items-center">
       <span class="material-symbols-outlined mr-3">
       work
     </span>
-    <input type="text" placeholder="Title, companies, expertise or benefits" class="w-[500px] focus:ring-blue-500 focus:border-blue-500">
+    <input type="text" placeholder="Title, companies, expertise or benefits" class="w-full focus:ring-blue-500 focus:border-blue-500 truncate">
     </div>
     
     <div class="bg-[#1E86FF] w-[146px] h-[47px] rounded text-white flex justify-center items-center cursor-pointer">
